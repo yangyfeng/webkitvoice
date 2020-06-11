508142bd47efb88146caef6d630c89e632aeb77b
‘杨玉峰’ <yangyufeng0423@qq.com>
2020-06-11 20:45:39 +0800

    修改样式

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 (function (e, p) {
-  console.log(e, p)
   var m = location.href.match(/platform=(win8|win|mac|linux|cros)/);
   e.id = (m && m[1]) ||
     (p.indexOf('Windows NT 6.2') > -1 ? 'win8' : p.indexOf('Windows') > -1 ? 'win' : p.indexOf('Mac') > -1 ? 'mac' : p.indexOf('CrOS') > -1 ? 'cros' : 'linux');
