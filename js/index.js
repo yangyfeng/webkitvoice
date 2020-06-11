fa90a7bed3655a5b830e054b440c3366d416b202
‘杨玉峰’ <yangyufeng0423@qq.com>
2020-06-11 20:17:05 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+(function (e, p) {
+  console.log(e, p)
+  var m = location.href.match(/platform=(win8|win|mac|linux|cros)/);
+  e.id = (m && m[1]) ||
+    (p.indexOf('Windows NT 6.2') > -1 ? 'win8' : p.indexOf('Windows') > -1 ? 'win' : p.indexOf('Mac') > -1 ? 'mac' : p.indexOf('CrOS') > -1 ? 'cros' : 'linux');
+  e.className = e.className.replace(/\bno-js\b/, 'js');
+})(document.documentElement, window.navigator.userAgent)
